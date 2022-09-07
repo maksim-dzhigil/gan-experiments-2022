bc5bee539ad4b17a2d47ffbf61e68463f3115254
K-mixam <maxdzhigil@gmail.com>
2022-09-07 17:00:15 +0500

    add files to distinguish branches

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name="gan-experiments-2022",
+      version="0.1dev",
+      author="maksim-dzhigil",
+      packages=['experiments', 'networks', 'tools', 'train_data'])
